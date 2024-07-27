0caef6dcdb487da49e2c4f8c14cb9c5589e6acb4
Courage Nduka <courageobunike@gmail.com>
2024-07-27 22:42:30 +0100

    making progress

--- Mecnix/src/page/Landingpage/Landingpage.jsx
+++ Mecnix/src/page/Landingpage/Landingpage.jsx
@@ -12,25 +12,20 @@ import meeting from "../../images/image 16.png";
 import cryptocafe from "../../images/cryptocafe.png";
 import readBlog from "../../images/Frame 33.svg";
 import Carousel from './ourPost';
-import slideImages from '../../images/Ftx.png'
+import slideImages from '../../images/Ftx.png';
 import Footer from '../../components/Footer';
 import ScrollComponent from './whatWeDo';
 import event from '../../images/event.png';
 import arrow from '../../images/arrow.png';
 
 const Landingpage = () => {
-  const [activeTab, setActiveTab] = useState(null);
-  const slides = [slideImages,slideImages, slideImages, slideImages]
-
-  const handleTabClick = (tabIndex) => {
-    setActiveTab(tabIndex === activeTab ? null : tabIndex); 
-  };
+   const slides = [slideImages, slideImages, slideImages, slideImages];
 
   return (
     <div>
       <NavigationBar />
       <div className="absolute inset-0 flex items-center justify-center md:-mt-16 md:ml-96 -mr-96 md:-mr-0 ">
-        <img src={Cube} className="h-4/6  md:ml-52 z-30 -mr-96 md:-mr-0 ml-40" alt="Cube" />
+        <img src={Cube} className="h-4/6 md:ml-52 z-30 -mr-96 md:-mr-0 ml-40" alt="Cube" />
       </div>
 
       <div className="absolute text-white -mt-60 font-Saira ml-32">
@@ -73,37 +68,39 @@ const Landingpage = () => {
           <ScrollComponent />
           <div>
             <div className='absolute bg-black mt-20 w-6/12 h-96 flex'>
-            <div>
-            <img src={cryptocafe} className='h-16 ml-24 mt-10'/>
-               <p className='text-3xl font-saira font-semibold mt-10 ml-24'> Catch Us live on Twitter space</p>
-               <p className='word-break w-96 ml-24 text-gray-600 font-saira mt-4'>Join our Twitter Space to stay updated on the latest happenings in the Web3 space. Don't miss out on insightful discussions and the chance to connect with industry experts.</p>
-               <button className='bg-textColor w-44 h-10 text-sm font-saira ml-24 rounded-lg mt-3'> Add to your Calender</button>
-            </div>
-            <div className='md:ml-96 ml-72 mt-10'>
-              <button className='w-20 h-10 text-lg font-medium ml-40'>
-                <img src={event}/>
-                <img src={arrow} className='mt-56'/>
-              </button>
-            </div>
+              <div>
+                <img src={cryptocafe} className='h-16 ml-24 mt-10' />
+                <p className='text-3xl font-saira font-semibold mt-10 ml-24'> Catch Us live on Twitter space</p>
+                <p className='word-break w-96 ml-24 text-gray-600 font-saira mt-4'>
+                  Join our Twitter Space to stay updated on the latest happenings in the Web3 space. Don't miss out on insightful discussions and the chance to connect with industry experts.
+                </p>
+                <button className='bg-textColor w-44 h-10 text-sm font-saira ml-24 rounded-lg mt-3'> Add to your Calendar</button>
+              </div>
+              <div className='md:ml-96 ml-72 mt-10'>
+                <button className='w-20 h-10 text-lg font-medium ml-40'>
+                  <img src={event} />
+                  <img src={arrow} className='mt-56' />
+                </button>
+              </div>
             </div>
             <div className='mt-20 ml-80 '>
-               <img src={meeting} className='h-96 w-11/12 ml-20'/>
+              <img src={meeting} className='h-96 w-11/12 ml-20' />
             </div>
           </div>
           <div className='flex w-10/12 ml-8'>
             <div className='mt-10'>
-               <img src={readBlog}/>
+              <img src={readBlog} />
             </div>
             <div className='w-3/12'>
-               <p className='word-break w-20 text-6xl font-bold mt-10 leading-tight ml-10'>READ OUR BLOGS</p>
-               <p className='bg-textColor w-32 h-10 md:mt-56 mt-24 ml-12 rounded text-center text-lg pt-1'>Visit Blog &rarr;</p>
+              <p className='word-break w-20 text-6xl font-bold mt-10 leading-tight ml-10'>READ OUR BLOGS</p>
+              <p className='bg-textColor w-32 h-10 md:mt-56 mt-24 ml-12 rounded text-center text-lg pt-1'>Visit Blog &rarr;</p>
             </div>
           </div>
-          <div >
-              <p className='mb-10 ml-20 text-2xl font-bold font-saira'>Recent Post</p>
-               <Carousel slides={slides} interval={3000}/>
-            </div>
-            <Footer/>
+          <div>
+            <p className='mb-10 ml-20 text-2xl font-bold font-saira'>Recent Post</p>
+            <Carousel slides={slides} interval={3000} />
+          </div>
+          <Footer />
         </div>
       </div>
     </div>
--- Mecnix/src/page/Landingpage/whatWeDo.jsx
+++ Mecnix/src/page/Landingpage/whatWeDo.jsx
@@ -11,7 +11,7 @@ import react from "../../images/react.png";
 const ScrollComponent = () => {
   useEffect(() => {
     AOS.init({
-      duration: 1000, 
+      duration: 1000,
       once: false,
       mirror: true,
     });
@@ -20,105 +20,100 @@ const ScrollComponent = () => {
   return (
     <div className="container mx-auto p-4">
       <section className="h-screen flex items-center justify-center">
-       <div>
-         <div>
-            <div className="flex ml-14">
+        <div>
+          <div className="flex ml-14">
             <span>
-            <img src={dropdown} className='h-40'/>
+              <img src={dropdown} className='h-40' />
             </span>
             <span className="mt-20 ml-5">
-                <img src={whatwedo} alt="What We Do" className="h-14" />
-                <p className="text-4xl font-bold font-Saira ">Project Development</p>
+              <img src={whatwedo} alt="What We Do" className="h-14" />
+              <p className="text-4xl font-bold font-Saira">Project Development</p>
             </span>
-            </div>
-         </div>
+          </div>
           <div className="border border-gray-600 rounded-3xl w-customWidth ml-36 h-full mt-20">
             <div className="bg-white bg-opacity-5 backdrop-filter backdrop-blur-sm h-40 rounded-t-3xl">
-                <p className="break-words font-Saira w-customWidth2 ml-20 pt-10 font-medium tracking-wide">
+              <p className="break-words font-Saira w-customWidth2 ml-20 pt-10 font-medium tracking-wide">
                 We develop projects that push the boundaries of innovation in the crypto space. Our expert team brings your ideas to life, ensuring they are built on solid technical foundations and designed to succeed. From conceptualization to deployment, we provide end-to-end development services tailored to meet your specific needs.
-                </p>
+              </p>
             </div>
             <img src={react} alt="React" className="mt-4" />
-            </div>
           </div>
+        </div>
       </section>
 
       <section data-aos="fade-up" className="h-screen flex items-center justify-center mt-20">
-      <div>
+        <div>
+          <div>
+            <p className="text-4xl font-bold font-Saira ml-32">Project Management and Marketing</p>
             <div>
-             <p className="text-4xl font-bold font-Saira ml-32">Project Management and Marketing</p>
-             <div>
-              <img src={ dropdown2} className='h-96 -mt-20 ml-1' />
+              <img src={dropdown2} className='h-96 -mt-20 ml-1' />
             </div>
-            </div>
-            
-              <>
-              <div className="p-4 -mt-32">
-                <div>
-                  <p className="break-words w-9/12 ml-28 text-gray-300 -mt-40 font-saira ">
-                    We provide holistic project management and marketing services that encompass every aspect of your project’s journey. Our offerings include:
-                  </p>
-                </div>
-                <div className="flex w-11/12">
-                  <span className="bg-white bg-opacity-5 backdrop-filter backdrop-blur-md font-saira text-base rounded-2xl border-gray-600 border h-52 w-4/12 ml-24 mt-5">
-                    <p className="mt-5 ml-10 font-saira font-semibold">Design</p>
-                    <p className="break-words w-64 ml-10 mt-10">
-                      We tell the right story by crafting visually compelling and user-friendly interfaces for your project.
-                    </p>
-                  </span>
-                  <span className="bg-white bg-opacity-5 backdrop-filter backdrop-blur-md font-saira  text-base rounded-2xl border-gray-600 border h-52 w-4/12 mt-5 ml-2">
-                    <p className="mt-5 ml-10 font-saira font-semibold">Writing</p>
-                    <p className="break-words w-64 ml-10 mt-10">
-                      Creating engaging content that resonates with your audience and helps your project to drive sales.
-                    </p>
-                  </span>
-                  <span className="bg-white bg-opacity-5 backdrop-filter backdrop-blur-md font-saira  text-base rounded-2xl border-gray-600 border h-52 w-4/12 mt-5 ml-2">
-                    <p className="mt-5 ml-10 font-saira font-semibold">Community Management</p>
-                    <p className="break-words w-64 ml-10 mt-10">
-                      We help web3 companies create, and manage their community to foster engagement and loyalty.
-                    </p>
-                  </span>
-                </div>
-                <div className="flex ml-20 w-10/12">
-                  <span className="bg-white bg-opacity-5 backdrop-filter backdrop-blur-md font-saira  text-base rounded-2xl border-gray-600 border h-52 w-4/12 mt-5 ml-2">
-                    <p className="mt-5 ml-10 font-saira font-semibold">Shilling</p>
-                    <p className="break-words w-68 ml-10 mt-10">
-                      Promoting your project through effective shilling strategies. Our team employs various tactics to generate buzz and increase visibility.
-                    </p>
-                  </span>
-                  <span className="bg-white bg-opacity-5 backdrop-filter backdrop-blur-md font-saira  text-base rounded-2xl border-gray-600 border h-52 w-4/12 mt-5 ml-2">
-                    <p className="mt-5 ml-10 font-saira font-semibold">KOLs</p>
-                    <p className="break-words w-64 ml-10 mt-10">
-                      Leveraging Key Opinion Leaders to boost your project's visibility and credibility.
-                    </p>
-                  </span>
-                </div>
-              </div>
-              </>
           </div>
+
+          <div className="p-4 -mt-32">
+            <div>
+              <p className="break-words w-9/12 ml-28 text-gray-300 -mt-40 font-saira">
+                We provide holistic project management and marketing services that encompass every aspect of your project’s journey. Our offerings include:
+              </p>
+            </div>
+            <div className="flex w-11/12">
+              <span className="bg-white bg-opacity-5 backdrop-filter backdrop-blur-md font-saira text-base rounded-2xl border-gray-600 border h-52 w-4/12 ml-24 mt-5">
+                <p className="mt-5 ml-10 font-saira font-semibold">Design</p>
+                <p className="break-words w-64 ml-10 mt-10">
+                  We tell the right story by crafting visually compelling and user-friendly interfaces for your project.
+                </p>
+              </span>
+              <span className="bg-white bg-opacity-5 backdrop-filter backdrop-blur-md font-saira text-base rounded-2xl border-gray-600 border h-52 w-4/12 mt-5 ml-2">
+                <p className="mt-5 ml-10 font-saira font-semibold">Writing</p>
+                <p className="break-words w-64 ml-10 mt-10">
+                  Creating engaging content that resonates with your audience and helps your project to drive sales.
+                </p>
+              </span>
+              <span className="bg-white bg-opacity-5 backdrop-filter backdrop-blur-md font-saira text-base rounded-2xl border-gray-600 border h-52 w-4/12 mt-5 ml-2">
+                <p className="mt-5 ml-10 font-saira font-semibold">Community Management</p>
+                <p className="break-words w-64 ml-10 mt-10">
+                  We help web3 companies create, and manage their community to foster engagement and loyalty.
+                </p>
+              </span>
+            </div>
+            <div className="flex ml-20 w-10/12">
+              <span className="bg-white bg-opacity-5 backdrop-filter backdrop-blur-md font-saira text-base rounded-2xl border-gray-600 border h-52 w-4/12 mt-5 ml-2">
+                <p className="mt-5 ml-10 font-saira font-semibold">Shilling</p>
+                <p className="break-words w-68 ml-10 mt-10">
+                  Promoting your project through effective shilling strategies. Our team employs various tactics to generate buzz and increase visibility.
+                </p>
+              </span>
+              <span className="bg-white bg-opacity-5 backdrop-filter backdrop-blur-md font-saira text-base rounded-2xl border-gray-600 border h-52 w-4/12 mt-5 ml-2">
+                <p className="mt-5 ml-10 font-saira font-semibold">KOLs</p>
+                <p className="break-words w-64 ml-10 mt-10">
+                  Leveraging Key Opinion Leaders to boost your project's visibility and credibility.
+                </p>
+              </span>
+            </div>
+          </div>
+        </div>
       </section>
 
       <section data-aos="fade-up" className="h-screen flex items-center justify-center">
-      <div>
-            <div>
-              <p className="text-4xl font-bold font-Saira  ml-28">Dev Onboarding Program</p>
-            </div>
-              <>
-              <div className=''>
-               <img src={dropdown3} className='h-96'/>
-               </div>
-              <div className="border border-gray-600 rounded-3xl w-customWidth ml-24 h-full -mt-80">
-                <div>
-                  <img src={devImage}/>
-                </div>
-                <div className="bg-white bg-opacity-5 backdrop-filter backdrop-blur-sm h-40 rounded-b-3xl">
-                  <p className="break-words font-Saira w-customWidth2 ml-20 pt-10 font-medium tracking-wide">
-                    Our Dev Onboarding Program is designed to help blockchain projects attract and integrate skilled developers into their ecosystems. We provide comprehensive support to ensure a smooth transition and productive collaboration. Our program includes: Developer Outreach, Training and Support, and Community Building.
-                  </p>
-                </div>
-              </div>
-              </>
+        <div>
+          <div>
+            <p className="text-4xl font-bold font-Saira ml-28">Dev Onboarding Program</p>
           </div>
+
+          <div>
+            <img src={dropdown3} className='h-96' />
+          </div>
+          <div className="border border-gray-600 rounded-3xl w-customWidth ml-24 h-full -mt-80">
+            <div>
+              <img src={devImage} />
+            </div>
+            <div className="bg-white bg-opacity-5 backdrop-filter backdrop-blur-sm h-40 rounded-b-3xl">
+              <p className="break-words font-Saira w-customWidth2 ml-20 pt-10 font-medium tracking-wide">
+                Our Dev Onboarding Program is designed to help blockchain projects attract and integrate skilled developers into their ecosystems. We provide comprehensive support to ensure a smooth transition and productive collaboration. Our program includes: Developer Outreach, Training and Support, and Community Building.
+              </p>
+            </div>
+          </div>
+        </div>
       </section>
     </div>
   );
