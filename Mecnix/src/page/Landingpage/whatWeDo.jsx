f98f99ace511c210b66c4d249c7c089f77d37ac7
Courage Nduka <courageobunike@gmail.com>
2024-08-21 14:47:50 +0100

    --fix:: made some small changes to the font

--- Mecnix/src/page/Landingpage/whatWeDo.jsx
+++ Mecnix/src/page/Landingpage/whatWeDo.jsx
@@ -28,12 +28,12 @@ const ScrollComponent = () => {
             </span>
             <span className="md:mt-20 md:ml-5">
               <img src={whatwedo} alt="What We Do" className="md:h-14 h-8" />
-              <p className="md:text-4xl text-2xl font-bold font-Saira -ml-1">Project Development</p>
+              <p className="md:text-4xl text-2xl font-bold font-saira -ml-1">Project Development</p>
             </span>
           </div>
           <div className="border border-gray-600 rounded-3xl md:w-customWidth w-customWidth6 md:ml-24 -ml-4  h-full md:mt-20 bg-black">
             <div className="bg-white bg-opacity-5 backdrop-filter backdrop-blur-sm h-40 rounded-t-3xl">
-              <p className="break-words font-Saira w-customWidth3 md:ml-10 md:pt-10 md:font-medium tracking-wide ml-3 md:text-lg text-sm pt-3 p-2">
+              <p className="break-words font-saira w-customWidth3 md:ml-10 md:pt-10 md:font-medium tracking-wide ml-3 md:text-lg text-sm pt-3 p-2">
                 We develop projects that push the boundaries of innovation in the crypto space. Our expert team brings your ideas to life, ensuring they are built on solid technical foundations and designed to succeed. From conceptualization to deployment, we provide end-to-end development services tailored to meet your specific needs.
               </p>
             </div>
@@ -45,7 +45,7 @@ const ScrollComponent = () => {
       <section data-aos="fade-up" className="h-screen flex items-center justify-center md:mt-20 mt-60 bg-black">
         <div>
         <div className='flex flex-row-reverse md:block'>
-            <p className="text-2xl md:text-4xl font-bold font-Saira -ml-10 md:ml-28">Project Management and Marketing</p>
+            <p className="text-2xl md:text-4xl font-bold font-saira -ml-10 md:ml-28">Project Management and Marketing</p>
             <div>
               <img src={dropdown2} className='hidden md:block h-96 md:-mt-20 md:ml-6 -ml-10'/>
               <img src={dot} className='md:hidden h-20 w-36 block -ml-10 -mt-5'/>
@@ -100,7 +100,7 @@ const ScrollComponent = () => {
       <section data-aos="fade-up" className="h-screen flex items-center justify-center bg-black md:mt-0 mt-96">
         <div>
           <div className='flex flex-row-reverse md:block mb-10'>
-            <p className="md:text-4xl font-bold font-Saira md:ml-28 text-2xl mr-5">Dev Onboarding Program</p>
+            <p className="md:text-4xl font-bold font-saira md:ml-28 text-2xl mr-5">Dev Onboarding Program</p>
             <img src={dot} className='md:hidden h-24 w-24 block -ml-10  -mt-8'/>
           </div>
           <div>
@@ -111,7 +111,7 @@ const ScrollComponent = () => {
               <img src={devImage} className=' h-auto w-full md:h-full'/>
             </div>
             <div className="bg-white bg-opacity-5 backdrop-filter backdrop-blur-sm md:h-40 h-72 rounded-b-3xl">
-              <p className="break-words font-Saira w-customWidth2 md:ml-20 ml-5 pt-7 md:font-medium tracking-wide text-base">
+              <p className="break-words font-saira w-customWidth2 md:ml-20 ml-5 pt-7 md:font-medium tracking-wide md:text-lg text-base">
                 Our Dev Onboarding Program is designed to help blockchain projects attract and integrate skilled developers into their ecosystems. We provide comprehensive support to ensure a smooth transition and productive collaboration. Our program includes: Developer Outreach, Training and Support, and Community Building.
               </p>
             </div>
